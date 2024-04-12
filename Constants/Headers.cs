baa96c5ee9f711328683e0f242b4c3ccee7a78f6
Anthony Larner <67445013+ajameslarner@users.noreply.github.com>
2024-04-12 11:28:52 +0100

    Revert "feat!(mvp): MVP Release for ApiContracts"

--- Constants/Headers.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiContracts.Constants;
-public static class Headers
-{
-    /// <summary>
-    /// The header name for the contract name.
-    /// </summary>
-    public const string ContractName = "Contract-Name";
-}
